8f5caa100ea46407132d12d6f7a922dbd25d6681
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-11 05:46:57 -0700

    added fade-in animation for the homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,27 @@ export default function Home() {
     <main className="flex-1 flex flex-col gap-8 items-start justify-start max-w-xl sm:pt-[7.825rem]">
       <div className="relative">
         <div className="relative">
-          {/* <div id="bulb" className={clsx(
-            "inline-block rounded-full py-[.4rem] px-[1.1rem] aspect-square border border-neutral-50 will-change-transform relative z-20",
-            styles.bulb
-          )}>❘</div> */}
           <div id="light" className={clsx(
             "absolute top-0 left-0 sm:top-24 sm:-left-4 origin-top blur-xl mix-blend-color-dodge z-10 pointer-events-none will-change-transform",
             styles.light
           )}></div>
         </div>
-        <h1 className="text-3xl text-neutral-50 font-kaiseiTokumin">
+        <h1 className={clsx(
+          "text-3xl text-neutral-50 font-kaiseiTokumin",
+          styles.fadein
+        )}>
           Abhijeet Singh
         </h1>
       </div>
       <div className="flex flex-col items-start justify-start gap-4 text-neutral-500 text-lg sm:text-base">
-        <p>Crafting <span className="text-neutral-50">engaging experiences</span> for the Internet.</p>
-        <p>With a creative approach to development, I add intuitiveness and life to my work. Building beautiful interfaces and performant backend applications for professional, academic, and personal projects.</p>
-        <p>When I&#39;m not busy writing code, I make digital artwork.</p>
+        <p className={styles.fadein}>Crafting <span className="text-neutral-50">engaging experiences</span> for the Internet.</p>
+        <p className={styles.fadein}>With a creative approach to development, I add intuitiveness and life to my work. Building beautiful interfaces and performant backend applications for professional, academic, and personal projects.</p>
+        <p className={styles.fadein}>When I&#39;m not busy writing code, I make digital artwork.</p>
       </div>
-      <div className="flex justify-between gap-8 w-max">
+      <div className={clsx(
+        "flex justify-between gap-8 w-max",
+        styles.fadein
+      )}>
         <Social />
       </div>
     </main>
