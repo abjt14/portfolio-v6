ad46e23ba707a8b9393564d93143ff4a8278d8f2
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-11 03:58:18 -0700

    updated navigation border animation and a css module file for it

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import styles from "@/styles/Navigation.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
@@ -34,10 +35,14 @@ export default function Navigation() {
   }
 
   return (
-    <aside className="sm:pl-8 sm:border-r sm:border-neutral-700">
+    <aside className={clsx(
+      "sm:pl-8 relative z-20",
+      styles.aside
+    )}>
       <nav className={clsx(
-        "sm:sticky sm:top-[12.25rem] pr-6",
-        "sm:after:content-[''] sm:after:absolute sm:after:top-[0%] sm:after:-right-[calc(.135rem)] sm:after:w-1 sm:after:h-1/4 sm:after:bg-neutral-50 sm:after:rounded-md sm:after:transition-all sm:after:duration-300 sm:after:transform-gpu",
+        "sm:sticky sm:top-[12.25rem] pr-6 z-30",
+        "sm:after:content-[''] sm:after:absolute sm:after:top-[0%] sm:after:-right-[calc(.135rem)] sm:after:w-1 sm:after:h-1/4 sm:after:rounded-md sm:after:transition-all sm:after:duration-300 sm:after:transform-gpu",
+        styles.pill,
         path === '/' && 'sm:after:top-[0%]',
         path === '/lab' && 'sm:after:top-[25%]',
         path === '/writing' && 'sm:after:top-[50%]',
@@ -45,12 +50,12 @@ export default function Navigation() {
       )}>
         <ul className="relative flex gap-6 justify-start items-center sm:flex-col sm:gap-0 sm:items-end sm:justify-between">
           {links.map(({ href, label }, index) => (
-            <li key={index} className="relative w-min py-0 sm:py-1">
+            <li key={index} className="relative w-min py-0 sm:py-1 sm:first:pt-0 sm:last:pb-0">
               <Link
                 href={href}
                 className={clsx(
-                  isActive(href) ? 'text-neutral-50 underline underline-offset-4 sm:no-underline' : 'text-neutral-500',
-                  'lowercase nav-link text-base font-kaiseiTokumin hover:text-neutral-50 transition-colors duration-150 will-change-auto'
+                  isActive(href) ? "text-neutral-50 underline underline-offset-4 sm:no-underline" : "text-neutral-500",
+                  "lowercase nav-link text-base font-kaiseiTokumin hover:text-neutral-50 transition-colors duration-150 will-change-auto"
                 )}
               >
                 {label}
