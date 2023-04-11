0ddb76ea40da1ad5c05dc807b7db25f703c5917b
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-11 05:47:43 -0700

    added fade-in animation and a beautiful line and pill animation for the nav component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -36,11 +36,11 @@ export default function Navigation() {
 
   return (
     <aside className={clsx(
-      "sm:pl-8 relative z-20",
+      "sm:relative sm:z-20 sm:pl-8 sm:after:will-change-auto",
       styles.aside
     )}>
       <nav className={clsx(
-        "sm:sticky sm:top-[12.25rem] pr-6 z-30",
+        "sm:z-30 sm:pr-6 sm:sticky sm:top-[12.25rem] sm:after:will-change-auto",
         "sm:after:content-[''] sm:after:absolute sm:after:top-[0%] sm:after:-right-[calc(.135rem)] sm:after:w-1 sm:after:h-1/4 sm:after:rounded-md sm:after:transition-all sm:after:duration-300 sm:after:transform-gpu",
         styles.pill,
         path === '/' && 'sm:after:top-[0%]',
@@ -50,12 +50,15 @@ export default function Navigation() {
       )}>
         <ul className="relative flex gap-6 justify-start items-center sm:flex-col sm:gap-0 sm:items-end sm:justify-between">
           {links.map(({ href, label }, index) => (
-            <li key={index} className="relative w-min py-0 sm:py-1 sm:first:pt-0 sm:last:pb-0">
+            <li key={index} className={clsx(
+              "relative w-min py-0 sm:py-1 sm:first:pt-0 sm:last:pb-0",
+              styles.fadein
+            )}>
               <Link
                 href={href}
                 className={clsx(
                   isActive(href) ? "text-neutral-50 underline underline-offset-4 sm:no-underline" : "text-neutral-500",
-                  "lowercase nav-link text-base font-kaiseiTokumin hover:text-neutral-50 transition-colors duration-150 will-change-auto"
+                  "lowercase nav-link text-base font-kaiseiTokumin transition-all duration-150 will-change-auto hover:text-neutral-50"
                 )}
               >
                 {label}
