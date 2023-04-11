f21d2fd463ecb28cd68efcdf96d5fedd53a8494d
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-11 03:59:56 -0700

    added an experiments data file and placeholder code

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,5 +1,19 @@
+import { experiments } from "@/data/experiments";
+
 export default function Experiments() {
   return (
-    <></>
+    <main className="max-w-xl sm:pt-[12.25rem]">
+      {experiments.map((experiment) => (
+        <Experiment key={experiment.id} {...experiment} />
+      ))}
+    </main>
+  )
+}
+
+function Experiment(experiment: Experiment) {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-bold">{experiment.name}</h1>
+    </div>
   )
 }
---
+++ src/data/experiments.ts
@@ -0,0 +1,32 @@
+export const experiments: Experiment[] = [
+  {
+    "id": "bongo-copy-cat",
+    "name": "Bongo Copy Cat",
+    "type": "external",
+    "link": "https://github.com/abjt14/bongo-copy-cat"
+  },
+  {
+    "id": "endless-burrito",
+    "name": "Endless Burrito",
+    "type": "external",
+    "link": "https://github.com/abjt14/endless-burrito"
+  },
+  {
+    "id": "border-highlight",
+    "name": "Border Highlight",
+    "type": "internal",
+    "link": "/lab/border-highlight"
+  },
+  {
+    "id": "laser-progress-beam",
+    "name": "Laser Progress Beam",
+    "type": "internal",
+    "link": "/lab/laser-progress-beam"
+  },
+  {
+    "id": "searchlight",
+    "name": "Searchlight",
+    "type": "internal",
+    "link": "/lab/searchlight"
+  },
+];
