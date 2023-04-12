7c93532890c35bd32c316883b7a6c317a04caa26
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-11 23:04:14 -0700

    fixed active link logic for navigation not working for slug pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { useEffect } from 'react';
 import styles from "@/styles/Navigation.module.css";
+import { useEffect } from 'react';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
@@ -32,49 +32,56 @@ export default function Navigation() {
     if (path === "/") {
       return href === path;
     }
-    return href.includes(path);
+    if (href !== links[0].href) {
+      return path.includes(href);
+    }
   }
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       document.documentElement.setAttribute('style', '--animation-delay-base: .25s !important');
-    }, 1500);
+    }, 3000);
     return () => clearTimeout(timeoutId);
   }, []);
 
   return (
     <aside className={clsx(
-      "sm:relative sm:z-20 sm:pl-8 sm:after:will-change-auto",
+      "sm:sticky sm:top-0 sm:z-20 sm:h-screen",
       styles.aside
     )}>
-      <nav className={clsx(
-        "sm:z-30 sm:pr-6 sm:sticky sm:top-[12.25rem] sm:after:will-change-auto",
-        "sm:after:content-[''] sm:after:absolute sm:after:top-[0%] sm:after:-right-[calc(.135rem)] sm:after:w-1 sm:after:h-1/4 sm:after:rounded-md sm:after:transition-all sm:after:duration-300 sm:after:transform-gpu",
-        styles.pill,
-        path === '/' && 'sm:after:top-[0%]',
-        path === '/lab' && 'sm:after:top-[25%]',
-        path === '/writing' && 'sm:after:top-[50%]',
-        path === '/resume' && 'sm:after:top-[75%]'
+      <div className={clsx(
+        "sm:relative sm:pl-8 sm:pt-[12.25rem] sm:after:will-change-auto sm:h-full",
+        styles.aside
       )}>
-        <ul className="relative flex gap-6 justify-start items-center sm:flex-col sm:gap-0 sm:items-end sm:justify-between">
-          {links.map(({ href, label }, index) => (
-            <li key={index} className={clsx(
-              "relative w-min py-0 sm:py-1 sm:first:pt-0 sm:last:pb-0",
-              styles.fadein
-            )}>
-              <Link
-                href={href}
-                className={clsx(
-                  isActive(href) ? "text-neutral-50 underline underline-offset-4 sm:no-underline" : "text-neutral-500",
-                  "lowercase nav-link text-base font-kaiseiTokumin transition-all duration-150 will-change-auto hover:text-neutral-50"
-                )}
-              >
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+        <nav className={clsx(
+          "sm:z-30 sm:pr-6 sm:sticky sm:after:will-change-auto",
+          "sm:after:content-[''] sm:after:absolute sm:after:top-[0%] sm:after:-right-[calc(.135rem)] sm:after:w-1 sm:after:h-1/4 sm:after:rounded-md sm:after:transition-all sm:after:duration-300 sm:after:transform-gpu",
+          styles.pill,
+          path === links[0].href && 'sm:after:top-[0%]',
+          path.includes(links[1].href) && 'sm:after:top-[25%]',
+          path.includes(links[2].href) && 'sm:after:top-[50%]',
+          path.includes(links[3].href) && 'sm:after:top-[75%]'
+        )}>
+          <ul className="relative flex gap-6 justify-start items-center sm:flex-col sm:gap-0 sm:items-end sm:justify-between">
+            {links.map(({ href, label }, index) => (
+              <li key={index} className={clsx(
+                "relative w-min py-0 sm:py-1 sm:first:pt-0 sm:last:pb-0",
+                styles.fadein
+              )}>
+                <Link
+                  href={href}
+                  className={clsx(
+                    isActive(href) ? "text-neutral-50 underline underline-offset-4 sm:no-underline" : "text-neutral-500",
+                    "lowercase nav-link text-base font-kaiseiTokumin transition-all duration-150 will-change-auto hover:text-neutral-50"
+                  )}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </aside>
   )
 }
