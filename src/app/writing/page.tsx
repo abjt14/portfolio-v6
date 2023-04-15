939ba355d71596dc38bfc013430fb5a13f25f420
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-15 05:48:40 -0700

    added basic styling to writing page

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,5 +1,6 @@
 export default function Writing() {
   return (
-    <></>
+    <section className="flex-1 max-w-full sm:pt-[12.25rem] flex flex-col md:flex-row gap-4 sm:gap-6 md:gap-1">
+    </section>
   )
 }
