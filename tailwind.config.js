35d09baa3d00174f82b80af5a867eec646ed8baa
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-04-15 05:36:18 -0700

    added a darker shade bg-neutral-1000 in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'neutral-1000': 'rgb(4, 4, 4)'
+      },
     },
   },
   plugins: [],
